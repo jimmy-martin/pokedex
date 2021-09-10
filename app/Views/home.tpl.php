3f4e676405895823cac6cc2ec0f29c7c149d4371
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-10 12:46:08 +0200

    update home page

--- app/Views/home.tpl.php
+++ app/Views/home.tpl.php
@@ -1,24 +1,50 @@
-<div class="card">
-    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/1.png" alt="Bulbizarre">
-    <h2 class="card__title">#1 Bulbizarre</h2>
-</div>
-<div class="card">
-    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/2.png" alt="Herbizarre">
-    <h2 class="card__title">#2 Herbizarre</h2>
-</div>
-<div class="card">
-    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/3.png" alt="Florizarre">
-    <h2 class="card__title">#3 Florizarre</h2>
-</div>
-<div class="card">
-    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/4.png" alt="Salamèche">
-    <h2 class="card__title">#4 Salamèche</h2>
-</div>
-<div class="card">
-    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/5.png" alt="Reptincel">
-    <h2 class="card__title">#5 Reptincel</h2>
-</div>
-<div class="card">
-    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/6.png" alt="Dracaufeu">
-    <h2 class="card__title">#6 Dracaufeu</h2>
+<div class="cards-group">
+    <div class="card">
+        <a href="#">
+            <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/1.png" alt="Bulbizarre">
+        </a>
+        <h2 class="card__title">
+            <a class="card__link" href="#">#1 Bulbizarre</a>
+        </h2>
+    </div>
+    <div class="card">
+        <a href="">
+            <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/2.png" alt="Herbizarre">
+        </a>
+        <h2 class="card__title">
+            <a class="card__link" href="#">#2 Herbizarre</a>
+        </h2>
+    </div>
+    <div class="card">
+        <a href="">
+            <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/3.png" alt="Florizarre">
+        </a>
+        <h2 class="card__title">
+            <a class="card__link" href="#">#3 Florizarre</a>
+        </h2>
+    </div>
+    <div class="card">
+        <a href="">
+            <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/4.png" alt="Salamèche">
+        </a>
+        <h2 class="card__title">
+            <a class="card__link" href="#">#4 Salamèche</a>
+        </h2>
+    </div>
+    <div class="card">
+        <a href="">
+            <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/5.png" alt="Reptincel">
+        </a>
+        <h2 class="card__title">
+            <a class="card__link" href="#">#5 Reptincel</a>
+        </h2>
+    </div>
+    <div class="card">
+        <a href="">
+            <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/6.png" alt="Dracaufeu">
+        </a>
+        <h2 class="card__title">
+            <a class="card__link" href="#">#6 Dracaufeu</a>
+        </h2>
+    </div>
 </div>
