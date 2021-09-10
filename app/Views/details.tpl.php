0fcd3dcd55eca4c1975af91308ee6d0d6e311425
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-10 13:50:16 +0200

    details style done

--- app/Views/details.tpl.php
+++ app/Views/details.tpl.php
@@ -1,45 +1,54 @@
-<h2>Détails de Herbizarre</h2>
-<div>
-    <img src="<?= $_SERVER['BASE_URI'] ?>/img/2.png" alt="Herbizarre">
-    <div>
-        <h3>#2 Herbizarre</h3>
-        <div>
-            Plante
-        </div>
-        <div>
-            Poison
-        </div>
-        <h4>Statistiques</h4>
-        <div>
-            <label for="pv">PV</label>
-            <span>60</span>
-            <progress id="pv" max="200" value="60"></progress>
-        </div>
-        <div>
-            <label for="attaque">Attaque</label>
-            <span>62</span>
-            <progress id="attaque" max="200" value="62"></progress>
-        </div>
-        <div>
-            <label for="defense">Défense</label>
-            <span>63</span>
-            <progress id="defense" max="200" value="63"></progress>
-        </div>
-        <div>
-            <label for="attaque-spe">Attaque Spé.</label>
-            <span>80</span>
-            <progress id="attaque-spe" max="200" value="80"></progress>
-        </div>
-        <div>
-            <label for="defense-spe">Défense Spé.</label>
-            <span>80</span>
-            <progress id="defense-spe" max="200" value="80"></progress>
-        </div>
-        <div>
-            <label for="vitesse">Vitesse</label>
-            <span>60</span>
-            <progress id="vitesse" max="200" value="60"></progress>
+<div class="detail">
+    <h2 class="detail__title">Détails de Herbizarre</h2>
+    <div class="detail__specs">
+        <img class="specs__img" src="<?= $_SERVER['BASE_URI'] ?>/img/2.png" alt="Herbizarre">
+        <div class="specs__details">
+            <h3 class="specs__title">#2 Herbizarre</h3>
+            <a href="#" class="specs__type-link">
+                Plante
+            </a>
+            <a href="#" class="specs__type-link">
+                Poison
+            </a>
+            <div class="specs__stats">
+                <h4 class="stats__title">Statistiques</h4>
+                <div class="stats-group">
+                    <div class="stat">
+                        <label for="pv">PV</label>
+                        <span>60</span>
+                        <progress id="pv" max="200" value="60"></progress>
+                    </div>
+                    <div class="stat">
+                        <label for="attaque">Attaque</label>
+                        <span>62</span>
+                        <progress id="attaque" max="200" value="62"></progress>
+                    </div>
+                    <div class="stat">
+                        <label for="defense">Défense</label>
+                        <span>63</span>
+                        <progress id="defense" max="200" value="63"></progress>
+                    </div>
+                    <div class="stat">
+                        <label for="attaque-spe">Attaque Spé.</label>
+                        <span>80</span>
+                        <progress id="attaque-spe" max="200" value="80"></progress>
+                    </div>
+                    <div class="stat">
+                        <label for="defense-spe">Défense Spé.</label>
+                        <span>80</span>
+                        <progress id="defense-spe" max="200" value="80"></progress>
+                    </div>
+                    <div class="stat">
+                        <label for="vitesse">Vitesse</label>
+                        <span>60</span>
+                        <progress id="vitesse" max="200" value="60"></progress>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
+    <div class="center">
+        <a class="detail__link" href="#">Revenir à la liste</a>
+    </div>
 </div>
-<a href="#">Revenir à la liste</a>
