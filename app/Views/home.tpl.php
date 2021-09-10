9d7be0c27f52d7001daf4640b3cfeca08a037820
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-10 12:27:17 +0200

    header style

--- app/Views/home.tpl.php
+++ app/Views/home.tpl.php
@@ -1,24 +1,24 @@
-<div>
-    <img src="<?= $_SERVER['BASE_URI'] ?>/img/1.png" alt="Bulbizarre">
-    <h2>#1 Bulbizarre</h2>
+<div class="card">
+    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/1.png" alt="Bulbizarre">
+    <h2 class="card__title">#1 Bulbizarre</h2>
 </div>
-<div>
-    <img src="<?= $_SERVER['BASE_URI'] ?>/img/2.png" alt="Herbizarre">
-    <h2>#2 Herbizarre</h2>
+<div class="card">
+    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/2.png" alt="Herbizarre">
+    <h2 class="card__title">#2 Herbizarre</h2>
 </div>
-<div>
-    <img src="<?= $_SERVER['BASE_URI'] ?>/img/3.png" alt="Florizarre">
-    <h2>#3 Florizarre</h2>
+<div class="card">
+    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/3.png" alt="Florizarre">
+    <h2 class="card__title">#3 Florizarre</h2>
 </div>
-<div>
-    <img src="<?= $_SERVER['BASE_URI'] ?>/img/4.png" alt="Salamèche">
-    <h2>#4 Salamèche</h2>
+<div class="card">
+    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/4.png" alt="Salamèche">
+    <h2 class="card__title">#4 Salamèche</h2>
 </div>
-<div>
-    <img src="<?= $_SERVER['BASE_URI'] ?>/img/5.png" alt="Reptincel">
-    <h2>#5 Reptincel</h2>
+<div class="card">
+    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/5.png" alt="Reptincel">
+    <h2 class="card__title">#5 Reptincel</h2>
 </div>
-<div>
-    <img src="<?= $_SERVER['BASE_URI'] ?>/img/6.png" alt="Dracaufeu">
-    <h2>#6 Dracaufeu</h2>
+<div class="card">
+    <img class="card__img" src="<?= $_SERVER['BASE_URI'] ?>/img/6.png" alt="Dracaufeu">
+    <h2 class="card__title">#6 Dracaufeu</h2>
 </div>
--- app/Views/partials/header.tpl.php
+++ app/Views/partials/header.tpl.php
@@ -5,20 +5,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= $_SERVER['BASE_URI'] ?>/css/reset.css">
     <link rel="stylesheet" href="<?= $_SERVER['BASE_URI'] ?>/css/style.css">
     <title>Pokédex</title>
 </head>
 
 <body>
     <div class="container">
-        <header>
-            <h1>
-                <a href="#">Pokédex</a>
+        <header class="header">
+            <h1 class="header__title">
+                <a class="header__link" href="#">Pokédex</a>
             </h1>
-            <nav>
-                <ul>
-                    <li>Liste</li>
-                    <li>Type</li>
+            <nav class="menu">
+                <ul class="menu__list">
+                    <li class="menu__item">
+                        <a class="menu__link" href="#">Liste</a>
+                    </li>
+                    <li class="menu__item">
+                        <a class="menu__link" href="#">Types</a>
+                    </li>
                 </ul>
             </nav>
         </header>
