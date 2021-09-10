9c41979bb2b6386f5a62a36dc7f8059a43e9bcdb
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-10 11:09:36 +0200

    views structure & show function

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -4,8 +4,27 @@ namespace Pokedex\Controllers;
 
 class MainController
 {
+    /**
+     * Displays our views
+     *
+     * @param string $viewName
+     * @param array $viewDatas
+     * @return void
+     */
+    protected function show(string $viewName, array $viewDatas = [])
+    {
+        extract($viewDatas);
+
+        require_once __DIR__ . '/../views/partials/header.tpl.php';
+        require_once __DIR__ . '/../views/' . $viewName . '.tpl.php';
+        require_once __DIR__ . '/../views/partials/footer.tpl.php';
+    }
+
+    /**
+     * Show home views
+     */
     public function home()
     {
-        echo 'Bonjour !';
+        $this->show('home');
     }
 }
