6547562bb6074e6aa95f3a5e25882fbc7d06489e
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-10 10:50:23 +0200

    router OK + installation var_dumper

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInitea4d5e482720a63dd19d4a573051f776
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitea4d5e482720a63dd19d4a573051f776::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireea4d5e482720a63dd19d4a573051f776($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequireea4d5e482720a63dd19d4a573051f776($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
